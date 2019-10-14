7f4111fc0b1ac75bf48fe760b8318e4a9b8a55d1
Frank Navarro <fnavarro7777@gmail.com>
2019-10-14 11:53:52 -0700

    Created navigating header

--- components/_App/Header.js
+++ components/_App/Header.js
@@ -1,5 +1,75 @@
+import { Menu, Container, Image, Icon } from 'semantic-ui-react';
+import Link from 'next/link';
+
 function Header() {
-  return <>Header</>;
+  const user = false;
+
+  return (
+    <Menu fluid id="menu" inverted>
+      <Container text>
+        <Link href="/">
+          <Menu.Item header>
+            <Image
+              size="mini"
+              src="/static/logo.svg"
+              style={{ marginRight: '1em' }}
+            />
+            React Reserve
+          </Menu.Item>
+        </Link>
+
+        <Link href="/cart">
+          <Menu.Item header>
+            <Icon name="cart" size="large" />
+            Cart
+          </Menu.Item>
+        </Link>
+
+        {user && (
+          <Link href="/create">
+            <Menu.Item header>
+              <Icon name="add square" size="large" />
+              Create
+            </Menu.Item>
+          </Link>
+        )}
+
+        {user ? (
+          <>
+            <Link href="/account">
+              <Menu.Item header>
+                <Icon name="user" size="large" />
+                Account
+              </Menu.Item>
+            </Link>
+
+            <Link href="/logout">
+              <Menu.Item header>
+                <Icon name="sign out" size="large" />
+                Logout
+              </Menu.Item>
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link href="/login">
+              <Menu.Item header>
+                <Icon name="sign in" size="large" />
+                Login
+              </Menu.Item>
+            </Link>
+
+            <Link href="/signup">
+              <Menu.Item header>
+                <Icon name="signup" size="large" />
+                Signup
+              </Menu.Item>
+            </Link>
+          </>
+        )}
+      </Container>
+    </Menu>
+  );
 }
 
 export default Header;
