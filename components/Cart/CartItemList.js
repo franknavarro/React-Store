3edcb5fd5557a20039f7c471f2fc66b0c6243846
Frank Navarro <fnavarro7777@gmail.com>
2019-10-15 08:34:42 -0700

    Template for cart created

--- components/Cart/CartItemList.js
+++ components/Cart/CartItemList.js
@@ -1,5 +1,25 @@
+import { Header, Segment, Button, Icon } from 'semantic-ui-react';
+
 function CartItemList() {
-  return <>CartItemList</>;
+  const user = false;
+
+  const renderUserButton = () => {
+    if (user) {
+      return <Button color="orange">View Products</Button>;
+    } else {
+      return <Button color="blue">Login to Add Products</Button>;
+    }
+  };
+
+  return (
+    <Segment secondary color="teal" inverted textAlign="center" placeholder>
+      <Header icon>
+        <Icon name="shopping basket" />
+        No products in your cart. Add some!
+      </Header>
+      <div>{renderUserButton()}</div>
+    </Segment>
+  );
 }
 
 export default CartItemList;
