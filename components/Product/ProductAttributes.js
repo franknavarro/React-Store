5607b68b4d185a09d9507a43f4fa73535a125cd1
Frank Navarro <fnavarro7777@gmail.com>
2019-10-14 18:40:04 -0700

    Can remove products now

--- components/Product/ProductAttributes.js
+++ components/Product/ProductAttributes.js
@@ -1,6 +1,19 @@
-import { Header, Button } from 'semantic-ui-react';
+import { useRouter } from 'next/router';
+import { Header, Button, Modal } from 'semantic-ui-react';
 
-function ProductAttributes({ description }) {
+import axios from '../../utils/axiosBase';
+
+function ProductAttributes({ description, _id }) {
+  const [modal, setModal] = React.useState(false);
+  const router = useRouter();
+
+  const handleDelete = async () => {
+    const url = '/product';
+    const payload = { params: { _id } };
+    await axios.delete(url, payload);
+    router.push('/');
+  };
+
   return (
     <>
       <Header as="h3">About this product</Header>
@@ -9,7 +22,24 @@ function ProductAttributes({ description }) {
         icon="trash alternate outline"
         color="red"
         content="Delete Product"
+        onClick={() => setModal(true)}
       />
+      <Modal open={modal} dimmer="blurring">
+        <Modal.Header>Confirm Delete</Modal.Header>
+        <Modal.Content>
+          <p>Are you sure you want to delete this product?</p>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button content="Cancel" onClick={() => setModal(false)} />
+          <Button
+            negative
+            icon="trash"
+            labelPosition="right"
+            content="Delete"
+            onClick={handleDelete}
+          />
+        </Modal.Actions>
+      </Modal>
     </>
   );
 }
--- pages/api/product.js
+++ pages/api/product.js
@@ -1,7 +1,28 @@
 import Product from '../../models/Product';
 
 export default async (req, res) => {
+  const { method } = req;
+
+  switch (method) {
+    case 'GET':
+      await handleGetRequest(req, res);
+      break;
+    case 'DELETE':
+      await handleDeleteRequest(req, res);
+      break;
+    default:
+      res.status(405).send(`Method ${method} not allowed`);
+  }
+};
+
+async function handleGetRequest(req, res) {
   const { _id } = req.query;
   const product = await Product.findById(_id);
   res.status(200).json(product);
-};
+}
+
+async function handleDeleteRequest(req, res) {
+  const { _id } = req.query;
+  await Product.findOneAndDelete({ _id });
+  res.status(204).json({});
+}
