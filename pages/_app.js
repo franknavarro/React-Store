af3674e4696554324e78804a1992d924387b1d7c
Frank Navarro <fnavarro7777@gmail.com>
2019-10-15 12:21:50 -0700

    Get user information with each page request

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,55 @@
 import App from 'next/app';
+import { parseCookies, destroyCookie } from 'nookies';
+
 import Layout from '../components/_App/Layout';
+import { redirectUser } from '../utils/auth';
+import axiosBase from '../utils/axiosBase';
+
+const SUPER_PROTECTED_ROUTES = ['/create'];
+const PROTECTED_ROUTES = ['/account', ...SUPER_PROTECTED_ROUTES];
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
+    const { token } = parseCookies(ctx);
+
     let pageProps = {};
 
     if (Component.getInitialProps) {
       pageProps = await Component.getInitialProps(ctx);
+    }
+
+    const redirectProtected = permissions => {
+      const { pathname } = ctx;
+      const isProtectedRoute = PROTECTED_ROUTES.includes(pathname);
+      if (!permissions && isProtectedRoute) {
+        return redirectUser(ctx, '/login');
+      }
+
+      const isSuperRoute = SUPER_PROTECTED_ROUTES.includes(pathname);
+      const isAdmin = permissions === 'admin';
+      const isRoot = permissions === 'root';
+      if (isSuperRoute && !isAdmin && !isRoot) {
+        return redirectUser(ctx, '/');
+      }
+    };
+
+    if (!token) {
+      redirectProtected();
+    } else {
+      try {
+        const response = await axiosBase.get('/account', {
+          headers: { Authorization: token },
+        });
+        const user = response.data;
+        redirectProtected(user.role);
+        pageProps.user = user;
+      } catch (err) {
+        console.error('Error getting current user', err);
+        //Throw out invalid token
+        destroyCookie(ctx, 'token');
+        // Redirect to login page
+        redirectProtected();
+      }
     }
 
     return { pageProps };
@@ -15,7 +58,7 @@ class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <Layout>
+      <Layout {...pageProps}>
         <Component {...pageProps} />
       </Layout>
     );
