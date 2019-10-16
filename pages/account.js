0752dc6ce26233142367a13b54330f2bb47bf247
Frank Navarro <fnavarro7777@gmail.com>
2019-10-16 11:50:03 -0700

    Getting orders from api

--- pages/account.js
+++ pages/account.js
@@ -1,13 +1,27 @@
+import { parseCookies } from 'nookies';
+
+import axiosBase from '../utils/axiosBase';
 import AccountHeader from '../components/Account/AccountHeader';
 import AccountOrders from '../components/Account/AccountOrders';
 
-const Account = ({ user }) => {
+const Account = ({ user, orders }) => {
+  console.log({ orders });
   return (
     <>
       <AccountHeader {...user} />
-      <AccountOrders />
+      <AccountOrders {...orders} />
     </>
   );
+};
+
+Account.getInitialProps = async ctx => {
+  const { token } = parseCookies(ctx);
+  if (!token) {
+    return { orders: [] };
+  }
+  const payload = { headers: { Authorization: token } };
+  const response = await axiosBase.get('/orders', payload);
+  return response.data;
 };
 
 export default Account;
---
+++ pages/api/orders.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+
+import Order from '../../models/Order';
+import connectDb from '../../utils/connectDb';
+
+connectDb();
+
+export default async (req, res) => {
+  if (!req.headers.authorization) {
+    return res.status(401).send('No authorization token');
+  }
+
+  try {
+    const { userId } = jwt.verify(
+      req.headers.authorization,
+      process.env.JWT_SECRET,
+    );
+    const orders = await Order.find({ user: userId }).populate({
+      path: 'products.product',
+      model: 'Product',
+    });
+    res.status(200).json({ orders });
+  } catch (err) {
+    console.error(err);
+    res.status(403).send('Please login again');
+  }
+};
