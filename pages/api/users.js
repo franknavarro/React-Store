1677935b1906ae2c37cc1e3492a8944fd208a6a0
Frank Navarro <fnavarro7777@gmail.com>
2019-10-17 15:41:18 -0700

    Added getting of users

---
+++ pages/api/users.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+
+import User from '../../models/User';
+import connectDb from '../../utils/connectDb';
+
+connectDb();
+
+export default async (req, res) => {
+  if (!req.headers.authorization) {
+    return res.status(401).send('No authorization token');
+  }
+
+  try {
+    const { userId } = await jwt.verify(
+      req.headers.authorization,
+      process.env.JWT_SECRET,
+    );
+    const users = await User.find({ _id: { $ne: userId } });
+    res.status(200).json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(403).send('Please login again');
+  }
+};
